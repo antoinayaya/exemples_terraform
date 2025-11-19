aab4749c3d45be27502bed16be77ee9b9163af0b
HeH-Gianni <gianni.tricarico@heh.be>
2025-11-19 16:43:51 +0100

    Code de base web app

---
+++ main.tf
@@ -0,0 +1,129 @@
+/*
+Cette configuration Terraform permet de mettre en place une application web basique sur AWS à l'aide d'une instance EC2 exécutant Nginx.
+Elle comprend les composants réseau nécessaires, tels qu'un VPC, un sous-réseau, une passerelle Internet et des groupes de sécurité.
+Des identifiants AWS sont requis pour appliquer cette configuration. Ils peuvent être définis à l'aide de variables d'environnement ou de l'interface CLI AWS.
+*/
+
+variable "name_label" {}
+
+variable "name_label" {
+  type = value
+  description = "string"
+  default = value
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_ssm_parameter" "amzn2_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+# NETWORKING #
+resource "aws_vpc" "app" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+}
+
+resource "aws_internet_gateway" "app" {
+  vpc_id = aws_vpc.app.id
+
+}
+
+resource "aws_subnet" "public_subnet1" {
+  cidr_block              = "10.0.0.0/24"
+  vpc_id                  = aws_vpc.app.id
+  map_public_ip_on_launch = true
+}
+
+# ROUTING #
+resource "aws_route_table" "app" {
+  vpc_id = aws_vpc.app.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app.id
+  }
+}
+
+resource "aws_route_table_association" "app_subnet1" {
+  subnet_id      = aws_subnet.public_subnet1.id
+  route_table_id = aws_route_table.app.id
+}
+
+# SECURITY GROUPS #
+# Nginx security group 
+resource "aws_security_group" "nginx_sg" {
+  name   = "nginx_sg"
+  vpc_id = aws_vpc.app.id
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# INSTANCES #
+resource "aws_instance" "nginx1" {
+  ami                    = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet1.id
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+  user_data_replace_on_change = true
+
+  user_data = <<EOF
+#! /bin/bash
+sudo amazon-linux-extras install -y nginx1
+sudo service nginx start
+sudo rm /usr/share/nginx/html/index.html
+sudo cat > /usr/share/nginx/html/index.html << 'WEBSITE'
+<html>
+<head>
+    <title>Taco Team Server</title>
+</head>
+<body style="background-color:#1F778D">
+    <p style="text-align: center;">
+        <span style="color:#FFFFFF;">
+            <span style="font-size:100px;">Welcome to the website! Have a &#127790;</span>
+        </span>
+    </p>
+</body>
+</html>
+WEBSITE
+EOF
+
+}
