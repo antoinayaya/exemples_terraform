bed0fd028cf4064f8872a25b949d142b2335d3f7
antoinayaya <antoine.mauroit@gmail.com>
2025-12-02 16:51:16 +0100

    Add output variables for EC2 instance DNS, VPC ID, and public subnet ID

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "aws_instance_public_dns" {
+  description = "Nom d'hôte DNS public de l'instance EC2"
+  value       = "http://${aws_instance.nginx1.public_dns}:${var.http_port}"
+}
+
+//Exercice
+output "vpc_id" {
+  description = "ID du VPC créé"
+  value       = aws_vpc.app.id
+}
+
+output "public_subnet_id" {
+  description = "ID du sous-réseau public"
+  value       = aws_subnet.public_subnet1.id
+}
